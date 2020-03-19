81317d1e970e309ed5ef4452e709da4dd3098931
Vladimir Kozlov <vkozloff@gmail.com>
2020-03-19 12:37:50 +0200

    Updated build script

--- nrf5.py
+++ nrf5.py
@@ -173,12 +173,15 @@ if softdevice_ver:
                         board.get("build.core"), "SDK",
                         "components", "softdevice", softdevice_ver,
                         "toolchain", "armgcc")
-    mcu_family = board.get("build.arduino.ldscript", "").split("_")[1]
+    mcu_family = board.get("build.ldscript", "").split("_")[1]
     for f in listdir(ldscript_dir):
         if f.endswith(mcu_family) and softdevice_ver in f.lower():
             ldscript_path = join(ldscript_dir, f)
 
-    if not ldscript_path:
+    if ldscript_path:
+        env.Replace(LDSCRIPT_PATH=ldscript_path)
+        print("Linker Script: %s" % ldscript_path)
+    else:
         print("Warning! Cannot find an appropriate linker script for the "
               "required softdevice!")
 
